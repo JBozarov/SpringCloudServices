04827da6a188b3ae15d14733b053c7f8e3b60bba
John Bozarov <jbozarov@teksystems.com>
2021-03-08 16:33:34 -0600

    Eureka server enabled

--- Eureka/AmazonServer/src/test/java/com/example/demo/AmazonServerApplicationTests.java
+++ Eureka/AmazonServer/src/test/java/com/example/demo/AmazonServerApplicationTests.java
@@ -2,8 +2,10 @@ package com.example.demo;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootTest
+@EnableEurekaServer
 class AmazonServerApplicationTests {
 
 	@Test
