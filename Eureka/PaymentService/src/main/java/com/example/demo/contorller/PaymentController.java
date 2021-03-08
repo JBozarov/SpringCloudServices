669deb0a3a8d97f45b224e23d381b2da7d535be8
John Bozarov <jbozarov@teksystems.com>
2021-03-08 16:44:05 -0600

    Payment controller finished

--- Eureka/PaymentService/src/main/java/com/example/demo/contorller/PaymentController.java
+++ Eureka/PaymentService/src/main/java/com/example/demo/contorller/PaymentController.java
@@ -1,11 +1,16 @@
 package com.example.demo.contorller;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/payment-provider")
 public class PaymentController {
 
-    public String payNow(int price) {
+    @GetMapping("/payNow/{price}")
+    public String payNow(@PathVariable int price) {
         return "Payment successful and the price is " + price;
-    } 
+    }
 }
